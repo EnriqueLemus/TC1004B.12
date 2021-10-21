65ff6e69dc4a0106f6516bfd5777d37fa0e52e0b
EnriqueLemus <83040316+EnriqueLemus@users.noreply.github.com>
2021-10-21 12:53:04 -0500

    Update fork.c

--- fork.c
+++ fork.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main(){
+  while(1){
   int pid = fork();
   if(pid==0){
     printf("Soy el proceso hijo");
   } else {
   printf("Soy el padre y mi hijo es d%", pid);
+  }
   }
   return 0;
 }  
